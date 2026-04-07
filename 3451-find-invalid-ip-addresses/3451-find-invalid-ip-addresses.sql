30a4bea44fb23eb7bc7a59cf0423420d7fff618e
sennee <137972957+sennee@users.noreply.github.com>
2026-04-07 11:08:23 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
+++ 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
@@ -10,10 +10,10 @@ SELECT  ip
                         OR CAST(REGEXP_SUBSTR(ip, '[0-9]+', 1, 2) AS UNSIGNED) > 255
                         OR CAST(REGEXP_SUBSTR(ip, '[0-9]+', 1, 3) AS UNSIGNED) > 255
                         OR CAST(REGEXP_SUBSTR(ip, '[0-9]+', 1, 4) AS UNSIGNED) > 255
-                        OR SUBSTRING(REGEXP_SUBSTR(ip, '[0-9]+', 1, 1), 1, 1) = '0'
-                        OR SUBSTRING(REGEXP_SUBSTR(ip, '[0-9]+', 1, 2), 1, 1) = '0'
-                        OR SUBSTRING(REGEXP_SUBSTR(ip, '[0-9]+', 1, 3), 1, 1) = '0'
-                        OR SUBSTRING(REGEXP_SUBSTR(ip, '[0-9]+', 1, 4), 1, 1) = '0'
+                        OR REGEXP_SUBSTR(ip, '[0-9]+', 1, 1) LIKE '0%'
+                        OR REGEXP_SUBSTR(ip, '[0-9]+', 1, 2) LIKE '0%'
+                        OR REGEXP_SUBSTR(ip, '[0-9]+', 1, 3) LIKE '0%'
+                        OR REGEXP_SUBSTR(ip, '[0-9]+', 1, 4) LIKE '0%'
                         OR REGEXP_SUBSTR(ip, '[0-9]+', 1, 4) IS NULL
                         OR REGEXP_SUBSTR(ip, '[0-9]+', 1, 5) IS NOT NULL
                         THEN 'invalid'
