53469e16eecafee727c19b62f8365032be6b3ac9
sennee <137972957+sennee@users.noreply.github.com>
2026-04-07 10:10:40 +0900

    Time: 280 ms (52.69%), Space: 0B (100%) - LeetHub

---
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT  transaction_date
+        , COALESCE(SUM(CASE WHEN MOD(amount, 2) <> 0 THEN amount END), 0) AS odd_sum
+        , COALESCE(SUM(CASE WHEN MOD(amount,2) = 0 THEN amount END), 0) AS even_sum
+  FROM  transactions
+ GROUP
+    BY  transaction_date
+ ORDER 
+    BY  transaction_date
